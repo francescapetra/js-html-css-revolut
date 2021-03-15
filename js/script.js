0276cd557f4ec4a51531910f68a6479c0c443321
francescapetra <francescalaurapetra@gmail.com>
2021-03-15 18:03:21 +0100

    shadow e margin dropmenu

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ var menuLink = $(".rigth_nav li ");
 
 menuLink.click(
   function(){
+    $(this).parent().find(".dropdown_menu").hide();
     $(this).children(".dropdown_menu").slideToggle();
   }
 );
 
-menuLink.mouseenter(
-  function(){
-    $(this).children(".dropdown_menu").show();
-  }
-);
-menuLink.mouseleave(
-  function(){
-    $(this).children(".dropdown_menu").hide();
-  }
-);
+// menuLink.mouseenter(
+//   function(){
+//     $(this).children(".dropdown_menu").show();
+//   }
+// );
+// menuLink.mouseleave(
+//   function(){
+//     $(this).children(".dropdown_menu").hide();
+//   }
+// );
